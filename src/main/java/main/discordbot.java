4d4429c54cd9e5694c7fe83023a8551a64a7c4f5
Tres Smith <tressmith@Tres.local>
2020-03-11 16:09:19 -0700

    fix: fix token and bot status to ONLINE

--- src/main/java/main/discordbot.java
+++ src/main/java/main/discordbot.java
@@ -11,8 +11,8 @@ import net.dv8tion.jda.api.entities.Activity;
 public class discordbot {
   public static JDA jda;
   public static void main(String[] args) throws LoginException {
-    jda = new JDABuilder(AccountType.BOT).setToken("").build();
-    jda.getPresence().setStatus(OnlineStatus.IDLE);
+    jda = new JDABuilder(AccountType.BOT).setToken("Njg3NDI2NDk4MDUwNzg1MzU1.Xmlt6w.AekFkFV-p18lOIqTB4jgOTSgCUs").build();
+    jda.getPresence().setStatus(OnlineStatus.ONLINE);
     jda.getPresence().setActivity(Activity.playing("Clash Royale"));
     jda.addEventListener(new GuildMemberJoin());
   }
