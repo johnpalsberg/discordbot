c0afc8e9608b01faae6adc58e8e81f6907dc9549
Tres Smith <tressmith@Tres.local>
2020-03-11 16:16:05 -0700

    fix: fix broken token

--- src/main/java/main/discordbot.java
+++ src/main/java/main/discordbot.java
@@ -11,7 +11,7 @@ import net.dv8tion.jda.api.entities.Activity;
 public class discordbot {
   public static JDA jda;
   public static void main(String[] args) throws LoginException {
-    jda = new JDABuilder(AccountType.BOT).setToken("Njg3NDI2NDk4MDUwNzg1MzU1.Xmlt6w.AekFkFV-p18lOIqTB4jgOTSgCUs").build();
+    jda = new JDABuilder(AccountType.BOT).setToken("Njg3NDI2NDk4MDUwNzg1MzU1.Xmlw7g.eWr6DcWUDT0u4YOK2nv4TdOFV-0").build();
     jda.getPresence().setStatus(OnlineStatus.ONLINE);
     jda.getPresence().setActivity(Activity.playing("Clash Royale"));
     jda.addEventListener(new GuildMemberJoin());
